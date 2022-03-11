f03964c53a27af2d9c9c478b57fd9d3ceee2806c
je0489 <je0489@naver.com>
2022-03-11 21:51:57 +0900

    Fix: Fix the allowed origin to several

--- src/Controller/controllers.js
+++ src/Controller/controllers.js
@@ -1,15 +1,17 @@
 const SHOWING_MSG = "안녕하세요장지은입니당";
 
 export const generator = (req, res) => {
-  const msg = SHOWING_MSG.match(/.{1,3}/g);
   const {
     params: { page },
+    headers: { origin },
   } = req;
+  const msg = SHOWING_MSG.match(/.{1,3}/g);
   const data = msg[page - 1];
   const hasNextPage = msg[page] !== undefined;
-  res.header({
-    "Access-Control-Allow-Origin": `${process.env.CLIENT_URL}`,
-    "Access-Control-Allow-Credentials": true,
-  });
+
+  const allowedOrigins = process.env.CLIENT_URL.split(";");
+  if (allowedOrigins.includes(origin))
+    res.setHeader("Access-Control-Allow-Origin", origin);
+  res.header("Access-Control-Allow-Credentials", true);
   return res.json({ data, hasNextPage });
 };
